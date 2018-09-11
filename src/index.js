c70a814139d19543ed0a33d8d990278500b2a413
Kuimov Andrey <a_kuimov@inbox.ru>
2018-09-11 14:32:40 +0700

    Add "eslint" and "travis"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-    console.log('Welcome to the Brain Games!');
-    const actual = readlineSync.question('May I have your name?: ');
-    console.log('Hello, ' + actual);
+  console.log('Welcome to the Brain Games!');
+  const actual = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${actual}`);
 };
