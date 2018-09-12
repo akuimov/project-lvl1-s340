660e3204414da39e60bfdd5b3dafa2499992bf18
Kuimov Andrey <a_kuimov@inbox.ru>
2018-09-12 11:20:53 +0700

    Create general function "brainGeneral"

---
+++ src/bin/brain-even.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import brainEven from '..';
+
+brainEven(2, 'Answer "yes" if number even otherwise answer "no".');
--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+const welcome = (question = '') => {
   console.log('Welcome to the Brain Games!');
+  if (question !== '') console.log(question);
   const actual = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${actual}`);
+  return actual;
 };
+
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const even = (score, user) => {
+  if (score < 1) {
+    console.log('Correct!');
+    return console.log(`Congratulations, ${user}!`);
+  }
+  const number = randomNumber(0, 100);
+  console.log(`Question: ${number}`);
+  const ansverUser = readlineSync.question('Your answer: ');
+  const realAnsver = number % 2 === 0 ? 'yes' : 'no';
+
+  if (number % 2 === 0 && (ansverUser !== realAnsver || ansverUser === 'no')) {
+    console.log(`${ansverUser} is wrong answer ;(. Correct answer was 'yes'.`);
+    return console.log(`Let's try again, ${user}!`);
+  } if (number % 2 !== 0 && (ansverUser !== realAnsver || ansverUser === 'yes')) {
+    console.log(`${ansverUser} is wrong answer ;(. Correct answer was 'no'.`);
+    return console.log(`Let's try again, ${user}!`);
+  }
+  const newScore = score - 1;
+  return even(newScore, user);
+};
+
+const brainEven = (order, description) => {
+  if (order === 1) {
+    welcome(description);
+  } else {
+    const welcomeName = welcome(description);
+    even(3, welcomeName);
+  }
+};
+
+export default brainEven;
