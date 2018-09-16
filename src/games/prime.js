bb85c888c503d7eb683eea35761c2541719218e9
Kuimov Andrey <a_kuimov@inbox.ru>
2018-09-16 18:50:50 +0700

    Task 9 recutsion

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,18 +3,14 @@ import { cons } from 'hexlet-pairs';
 
 const randomNum = () => Math.floor(Math.random() * (10 - 1) + 1);
 
-const prime = (n) => {
-  if (n < 2) return false;
-
-  for (let i = 2; i * i <= n; i += 1) {
-    if (n % i === 0) return false;
-  }
-  return true;
+const prime = (n, i) => {
+  if (i === 1) return true;
+  return n % i === 0 ? false : prime(n, i - 1);
 };
 
 const questionAndAnswer = () => {
   const number = randomNum();
-  const rez = prime(number) ? 'yes' : 'no';
+  const rez = prime(number, number - 1) ? 'yes' : 'no';
   return cons(number, rez);
 };
 
