45f44004744860a4f13a59e6ff9ec6696c050a8b
Kuimov Andrey <a_kuimov@inbox.ru>
2018-09-17 15:26:15 +0700

    fix function prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,14 +3,18 @@ import { cons } from 'hexlet-pairs';
 
 const randomNum = () => Math.floor(Math.random() * (10 - 1) + 1);
 
-const prime = (n, i) => {
-  if (i === 1) return true;
-  return n % i === 0 ? false : prime(n, i - 1);
+const prime = (n) => {
+  if (n < 2) return true;
+  const iter = (num, i) => {
+    if (i === 1) return true;
+    return n % i === 0 ? false : iter(n, i - 1);
+  };
+  return iter(n, n - 1);
 };
 
 const questionAndAnswer = () => {
   const number = randomNum();
-  const rez = prime(number, number - 1) ? 'yes' : 'no';
+  const rez = prime(number) ? 'yes' : 'no';
   return cons(number, rez);
 };
 
